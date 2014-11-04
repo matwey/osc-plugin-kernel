f6e4d9ae24dca084817c0e1509feb9cf89726754
Matwey V. Kornilov <matwey@sai.msu.ru>
2014-11-04 17:15:32 +0300

    Cache kernel package version, pretify output

--- osc-kernel.py
+++ osc-kernel.py
@@ -15,11 +15,14 @@ def get_kernel_version(self, project, package, repository, arch):
 def get_kernel_project(self, project):
 	ret = {}
 	fails = []
+	version = None
 
 	for x in self.get_kernel_package(project):
 		out = x['code']
 		if x['code'] == 'succeeded':
-			out = self.get_kernel_version(x['prj'], x['pac'], x['rep'], x['arch'])
+			if version == None:
+				version = self.get_kernel_version(x['prj'], x['pac'], x['rep'], x['arch'])
+			out = version
 		if x['code'] == 'failed':
 			fails.append(self.log_url(x['prj'], x['rep'], x['arch'], x['pac']))
 		if x['code'] == 'disabled':
@@ -32,7 +35,7 @@ def format_table(self, archs, projects):
 	a = max([len(x) for x in archs])
 	p = [len(x) for x in projects]
 	w = [a] + p
-	return "\t".join(['{{{0}:>{1}}}'.format(*x) for x in enumerate(w)])
+	return "\t".join(['{{{0}:<{1}}}'.format(*x) for x in enumerate(w)])
 
 @cmdln.alias('ks')
 def do_kernelsummary(self, subcmd, opts):
@@ -51,6 +54,7 @@ def do_kernelsummary(self, subcmd, opts):
 
 	res = dict([(p, self.get_kernel_project(p)) for p in _projects])
 
+	print "Kernel summary:"
 	print fmt.format("",*_projects)
 	for a in _archs:
 		print fmt.format(a,*([res[p][0].get(a,"") for p in _projects]))
